a38a998116908e72425dfe99dc39519f7b5d80f4
budko <budkovalexandr@gmail.com>
2023-01-26 22:03:22 +0300

    урок 2 выполнен. На проверку, с дополнениями

--- src/lesson2.java
+++ src/lesson2.java
@@ -10,6 +10,7 @@ public class lesson2 {
         task8();
     }
     public static void task1() {
+        System.out.println("Задача 1");
         byte a = 1;
         System.out.println("Значение переменной 'a' с типом" + " 'byte' равно " + a);
         short b = 10;
@@ -24,6 +25,7 @@ public class lesson2 {
         System.out.println("Значение переменной 'h' с типом" + " 'double' равно " + h);
     }
     public static void task2() {
+        System.out.println("Задача 2");
         float m = 27.12f;
         long v = 987_678_965_549L;
         byte j = 2;
@@ -34,6 +36,7 @@ public class lesson2 {
         byte l = 67;
     }
     public static void task3() {
+        System.out.println("Задача 3");
         int lP = 23; // Людмила Павловна
         int aS = 27; // Анна Сергеевна
         int eA = 30; // Екатерина Андреевна
@@ -41,26 +44,29 @@ public class lesson2 {
         System.out.println("На каждого ученика рассчитано " + listEachStudent + " листов бумаги");
     }
     public static void task4() {
+        System.out.println("Задача 4");
         byte bottle = 16;
         byte time = 2;
+        int bottlePerOneMinutes = bottle / time;
 
         byte min = 20;
-        byte time1 = (byte) (bottle * 20); // 20 minutes
+        int time1 = bottlePerOneMinutes * min; // 20 minutes
         System.out.println("За " + min + " минут, машина произвела " + time1 + " бутылок");
 
-        byte day1 = 1;
-        int time2 = (bottle * (24 * 60)); // 1 day
+        byte day1 = 24; // часа
+        int time2 = bottlePerOneMinutes * day1 * 60; // 1 day, 60 = в 1 часе минут
         System.out.println("За " + day1 + " день, машина произвела " + time2 + " бутылок");
 
         byte day3 = 3;
-        int time3 = (bottle * (24 * 60 * 3)); //3 day
+        int time3 = 3 * time2; //3 day
         System.out.println("За " + day3 + " дня, машина произвела " + time3 + " бутылок");
 
-        byte month = 1;
-        int time4 = (bottle * (24 * 60 * 30)); // 30 day
-        System.out.println("За " + month + " месяц, машина произвела " + time4 + " бутылок");
+        byte oneMonth = 1;
+        int time4 = 30 * time2; // 30 day
+        System.out.println("За " + oneMonth + " месяц, машина произвела " + time4 + " бутылок");
     }
     public static void task5() {
+        System.out.println("Задача 5");
         byte totalPaint = 120; // на 1 класс 2 банки белой краски и 4 банки коричневой
         byte whiteColor = 2;
         byte brownColor = 4;
@@ -71,24 +77,25 @@ public class lesson2 {
         System.out.println("В школе, где " + totalClass + " классов, нужно " + totalWhitePaint + " банок белой краски и " + totalBrownPaint + " банок коричневой краски.");
     }
     public static void task6() {
-        var bananas = 5;
-        var weightOneBanan = 80; // gramms
-        var totalWeigtsBananas = bananas * weightOneBanan;
+        System.out.println("Задача 6");
+        int bananas = 5;
+        int weightOneBanan = 80; // gramms
+        int totalWeigtsBananas = bananas * weightOneBanan;
         System.out.println("Общий вес бананов " + totalWeigtsBananas + " грамм.");
 
-        var milk = 200; // мл, 100 мл = 105 грамм
-        var totalWeigtMilk = milk / 100 * 105;
+        int milk = 200; // мл, 100 мл = 105 грамм
+        int totalWeigtMilk = milk / 100 * 105;
         System.out.println("Общий вес молока " + totalWeigtMilk + " грамм.");
 
-        var icecreamPlombir = 2; // каждый брикет = 100 грамм
-        var totalWeightIcecreamPlombir = icecreamPlombir * 100;
+        int icecreamPlombir = 2; // каждый брикет = 100 грамм
+        int totalWeightIcecreamPlombir = icecreamPlombir * 100;
         System.out.println("Общий вес мороженного - пломбир " + totalWeightIcecreamPlombir + " грамм.");
 
-        var eggs = 4; // 1 яйцо = 70 грамм
-        var totalWeightEggs = eggs * 70;
+        int eggs = 4; // 1 яйцо = 70 грамм
+        int totalWeightEggs = eggs * 70;
         System.out.println("Общий вес яиц " + totalWeightEggs + " грамм.");
 
-        var totalProductWeights = totalWeigtsBananas + totalWeigtMilk + totalWeightIcecreamPlombir + totalWeightEggs;
+        int totalProductWeights = totalWeigtsBananas + totalWeigtMilk + totalWeightIcecreamPlombir + totalWeightEggs;
         System.out.println("Общий вес продуктов " + totalProductWeights + " грамм.");
         // перевести граммы в кг, в 1 кг = 1000 грамм
         int grPerKg = 1000;
@@ -97,51 +104,53 @@ public class lesson2 {
         System.out.println("Общий вес продуктов " + totalProductWeightsKg + " кг.");
     }
     public static void task7() {
-        var weightKg = 7; // нужно сбросить 7 кг
-        var weightGr = weightKg * 1000;
-        var days = weightGr / 250; //Тренер скорректировал рацион так, чтобы спортсмен мог терять в весе 250 грамм в день.
+        System.out.println("Задача 7");
+        int weightKg = 7; // нужно сбросить 7 кг
+        int weightGr = weightKg * 1000;
+        int days = weightGr / 250; //Тренер скорректировал рацион так, чтобы спортсмен мог терять в весе 250 грамм в день.
         System.out.println("Потребуется " + days + " дней, чтоб похудеть на 7 кг.");
 
-        var days2 = weightGr/500; //Тренер скорректировал рацион так, чтобы спортсмен мог терять в весе 500 грамм в день.
+        int days2 = weightGr/500; //Тренер скорректировал рацион так, чтобы спортсмен мог терять в весе 500 грамм в день.
         System.out.println("Потребуется " + days2 + " дней, чтоб похудеть на 7 кг.");
 
-        var day3 = (days+days2) / 2;
+        int day3 = (days+days2) / 2;
         System.out.println("Потребуется " + day3 + " день в среднем, чтобы добиться результата похудения");
         //может потребоваться дней в среднем, чтобы добиться результата похудения
     }
     public static void task8() {
-        var masha = 67_760; // рублей в месяц
-        var denis = 83_690; //рублей в месяц
-        var cristy = 76_230;// рублей в месяц
+        System.out.println("Задача 8");
+        int masha = 67_760; // рублей в месяц
+        int denis = 83_690; //рублей в месяц
+        int cristy = 76_230;// рублей в месяц
 
         // Каждому нужно увеличить зарплату на 10% от текущей месячной
-        var procent = 10;
-        var salaryIncrease = masha / procent;
-        var mashaSalary = masha + salaryIncrease;
+        int procent = 10;
+        int salaryIncrease = masha / procent;
+        int mashaSalary = masha + salaryIncrease;
         System.out.println("Зарплата Маши после повышения " + mashaSalary + " рублей");
 
-        var salaryIncrease1 = denis / procent;
-        var denisSalary = denis + salaryIncrease1;
+        int salaryIncrease1 = denis / procent;
+        int denisSalary = denis + salaryIncrease1;
         System.out.println("Зарплата Дениса после повышения " + denisSalary + " рублей");
 
-        var salaryIncrease2 = cristy / procent;
-        var cristySalary = cristy + salaryIncrease2;
+        int salaryIncrease2 = cristy / procent;
+        int cristySalary = cristy + salaryIncrease2;
         System.out.println("Зарплата Кристины после повышения " + cristySalary + " рублей");
 
         //   Дополнительно руководитель попросил посчитать разницу между годовым доходом с нынешней зарплатой и после повышения
-        var annualProfit = masha*12; // годовой доход до повышения
-        var annualProfitNew = mashaSalary*12; // годовой доход после повышения
-        var difannualProfit = annualProfitNew - annualProfit;
+        int annualProfit = masha*12; // годовой доход до повышения
+        int annualProfitNew = mashaSalary*12; // годовой доход после повышения
+        int difannualProfit = annualProfitNew - annualProfit;
         System.out.println("Маша теперь получает " + mashaSalary + " рублей. Годовой доход вырос на " + difannualProfit + " рублей.");
 
-        var annualProfitDenis = denis*12; // годовой доход до повышения
-        var annualProfitNewDenis = denisSalary*12; // годовой доход после повышения
-        var difannualProfitDenis = annualProfitNewDenis - annualProfitDenis;
+        int annualProfitDenis = denis*12; // годовой доход до повышения
+        int annualProfitNewDenis = denisSalary*12; // годовой доход после повышения
+        int difannualProfitDenis = annualProfitNewDenis - annualProfitDenis;
         System.out.println("Денис теперь получает " + denisSalary + " рублей. Годовой доход вырос на " + difannualProfitDenis + " рублей.");
 
-        var annualProfitCristy = cristy*12; // годовой доход до повышения
-        var annualProfitNewCristy = cristySalary*12; // годовой доход после повышения
-        var difannualProfitCristy = annualProfitNewCristy - annualProfitCristy;
+        int annualProfitCristy = cristy*12; // годовой доход до повышения
+        int annualProfitNewCristy = cristySalary*12; // годовой доход после повышения
+        int difannualProfitCristy = annualProfitNewCristy - annualProfitCristy;
         System.out.println("Кристина теперь получает " + cristySalary + " рублей. Годовой доход вырос на " + difannualProfitCristy + " рублей.");
 
     }
